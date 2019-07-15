8edfcb9796a90475c74f3acb9962821d7013f404
goddda <savickaite.goda@gmail.com>
2019-07-15 15:15:01 +0300

    FIrst commit

---
+++ test2/Math.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace test2
+{
+    public class Math
+    {
+        // public static int myconstant = 5;
+        public int Sum(int n1, int n2)
+        {
+            int n = n1 + n2;
+            return n;
+        }
+
+        public int Subtract(int n1, int n2)
+        {
+            int n = n1 - n2;
+            return n;
+        }
+    }
+}
---
+++ test2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var math = new Math();
+            int n1, n2;
+            Console.WriteLine("Enter two numbers:");
+            n1 = int.Parse(Console.ReadLine());
+            n2 = int.Parse(Console.ReadLine());
+            var n3 = math.Sum(n1,n2);
+            var n4 = math.Subtract(n1, n2);
+            Console.WriteLine($"Sum: {n1}+{n2}={n3}");
+            Console.WriteLine($"Subtraction: {n1}-{n2}={n4}");
+
+            /* Static */
+            // var myconst = Math.myconstant;
+        }
+    }
+}
